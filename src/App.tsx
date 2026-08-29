54d3d35a23d86ae3b283c5caaf329b991d4bcce6
alexxaman123-lgtm <alexxaman123@gmail.com>
2026-08-30 03:03:33 +0530

    Keep app layout unchanged while adding authenticated header state

--- src/App.tsx
+++ src/App.tsx
@@ -14,35 +14,15 @@ export default function App() {
     <div className="relative min-h-screen">
       <BackgroundEffects />
       <Header />
-
       <main>
         <Hero />
-
-        <div className="mx-auto max-w-6xl px-4 pb-16 sm:px-6 lg:px-8">
-          <TypingTester />
-        </div>
-
-        <div className="border-t border-white/8 below-fold-content">
-          <SeoContent />
-        </div>
-
-        <div className="border-t border-white/8 below-fold-content">
-          <HowItWorks />
-        </div>
-
-        <div className="border-t border-white/8 below-fold-content">
-          <GuidesSection />
-        </div>
-
-        <div className="border-t border-white/8 below-fold-content">
-          <AboutSection />
-        </div>
-
-        <div className="border-t border-white/8 below-fold-content">
-          <FaqSection />
-        </div>
+        <div className="mx-auto max-w-6xl px-4 pb-16 sm:px-6 lg:px-8"><TypingTester /></div>
+        <div className="border-t border-white/8 below-fold-content"><SeoContent /></div>
+        <div className="border-t border-white/8 below-fold-content"><HowItWorks /></div>
+        <div className="border-t border-white/8 below-fold-content"><GuidesSection /></div>
+        <div className="border-t border-white/8 below-fold-content"><AboutSection /></div>
+        <div className="border-t border-white/8 below-fold-content"><FaqSection /></div>
       </main>
-
       <Footer />
     </div>
   );
