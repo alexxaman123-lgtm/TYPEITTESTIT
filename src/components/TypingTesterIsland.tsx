b345dc337d36983a8ef0440162aab94e86558d34
alexxaman123-lgtm <alexxaman123@gmail.com>
2026-09-03 09:50:40 +0530

    Pass locale into React typing app

--- src/components/TypingTesterIsland.tsx
+++ src/components/TypingTesterIsland.tsx
@@ -1,10 +1,11 @@
+import type { Locale } from "../lib/i18n";
 import TypingTester from "./TypingTester";
 import SecurityBoundary from "./SecurityBoundary";
 
-export default function TypingTesterIsland() {
+export default function TypingTesterIsland({ locale = "en" }: { locale?: Locale }) {
   return (
     <SecurityBoundary>
-      <TypingTester />
+      <TypingTester locale={locale} />
     </SecurityBoundary>
   );
 }
